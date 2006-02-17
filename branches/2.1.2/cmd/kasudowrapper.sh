c60926ec8d42b1ac2a0ea07ede4eaf8572f41976
jpeyrard <jpeyrard@ea33747e-1306-0410-90f0-ed196d91190f>
2006-02-17 13:06:06 +0000

    suppression des -l dans kasudowrapper, modification des scripts pour l'adaptation

--- branches/2.1.2/cmd/kasudowrapper.sh
+++ branches/2.1.2/cmd/kasudowrapper.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-DEPLOYETCDIR="/etc/kadeploy/"
-EXPORTENV="exported.conf";
-EXPORTENVFILE="$DEPLOYETCDIR/$EXPORTENV"
+DEPLOYDIR=/opt/kadeploy/
+DEPLOYUSER=deploy
+PERL5LIBDEPLOY=$DEPLOYDIR/share/perl/5.8
 
-OK=0
-if [ ! -d $DEPLOYETCDIR ] ; then echo "You don't have $DEPLOYETCDIR"; exit 1; fi
-
-if [ -f $EXPORTENVFILE ] ; then 
-    source $EXPORTENVFILE ; 
-else 
-    echo "$EXPORTENVFILE not found... you have to build it with kasetup";
-    exit 1;
-fi
-
-if [ -z $DEPLOYDIR ] ; then echo "DEPLOYDIR not found in $EXPORTENVFILE" ; exit 1 ; fi
-if [ -z $DEPLOYUSER ] ; then echo "DEPLOYUSER not found in $EXPORTENVFILE" ; exit 1 ; fi
-
-
-export PERL5LIB=${DEPLOYDIR}/:$PERL5LIB
+export PERL5LIB=${PERL5LIBDEPLOY}/:$PERL5LIB
 export DEPLOYDIR
 
-if [ $(basename $0) == "kadeploy" ] ; then  exec sudo -u $DEPLOYUSER $DEPLOYDIR/bin/`basename $0` -l $USER "$@" ; $OK=1 ;  fi
-if [ $(basename $0) == "kaenvironments" ] ; then exec sudo -u $DEPLOYUSER $DEPLOYDIR/bin/`basename $0` -l $USER "$@" ; $OK=1 ; fi
-if [ $(basename $0) == "karecordenv" ]   ; then exec sudo -u $DEPLOYUSER $DEPLOYDIR/bin/`basename $0` -l $USER "$@" ; $OK=1 ; fi
-if [ $(basename $0) == "setup_pxe.pl" ] ; then  exec sudo -u $DEPLOYUSER $DEPLOYDIR/sbin/`basename $0` "$@" ; $OK=1 ;  fi
-if [ $OK==0 ]; then exec sudo -u $DEPLOYUSER $DEPLOYDIR/bin/`basename $0` "$@" ; fi
+
+if [ -x $DEPLOYDIR/bin/`basename $0` ] ; then exec sudo -u $DEPLOYUSER $DEPLOYDIR/bin/`basename $0` "$@" ; $OK=1 ;  fi
+if [ -x $DEPLOYDIR/sbin/`basename $0` ] ; then exec sudo -u $DEPLOYUSER $DEPLOYDIR/sbin/`basename $0` "$@" ; $OK=1 ;  fi
+
+
+#if [ $(basename $0) == "setup_pxe.pl" ] ; then  exec sudo -u $DEPLOYUSER $DEPLOYDIR/sbin/`basename $0` "$@" ; $OK=1 ;  fi
+#if [ $OK==0 ]; then exec sudo -u $DEPLOYUSER $DEPLOYDIR/bin/`basename $0` "$@" ; fi
