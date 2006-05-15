3a61f584e3bf7adaa2f734d9c91054000bad4b0b
jpeyrard <jpeyrard@ea33747e-1306-0410-90f0-ed196d91190f>
2006-05-15 10:32:26 +0000

    synchronisation avec la branche alpha

---
+++ branches/2.2/bin/kasudowrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DEPLOYDIR=/opt/kadeploy/
+DEPLOYUSER=deploy
+PERL5LIBDEPLOY=$DEPLOYDIR/share/perl/5.8
+
+export PERL5LIB=${PERL5LIBDEPLOY}/:$PERL5LIB
+export DEPLOYDIR
+
+
+if [ -x $DEPLOYDIR/bin/`basename $0` ] ; then exec sudo -u $DEPLOYUSER $DEPLOYDIR/bin/`basename $0` "$@" ; $OK=1 ;  fi
+if [ -x $DEPLOYDIR/sbin/`basename $0` ] ; then exec sudo -u $DEPLOYUSER $DEPLOYDIR/sbin/`basename $0` "$@" ; $OK=1 ;  fi
+if [ ! $OK ] ; then echo "kasudowrapper.sh badly configured, use (prefix)/sbin/kasetup -exportenv" ; fi
+
