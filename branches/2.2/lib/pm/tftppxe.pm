8b080dbc7508024058a9eec45ae4b8fb82f818e4
jpeyrard <jpeyrard@ea33747e-1306-0410-90f0-ed196d91190f>
2006-06-05 14:00:53 +0000

    suppression de bug de path dans pxe

--- branches/2.2/lib/pm/tftppxe.pm
+++ branches/2.2/lib/pm/tftppxe.pm
@@ -1,7 +1,13 @@
-package tftppxe;
+package libkadeploy2::tftppxe;
 
-sub createtftp()
+use libkadeploy2::deployconf;
+use strict;
+use warnings;
+
+sub createtftp($)
 {
+    my $conf=shift; # configuration object
+
     my $kadeployuser;
     my $kadeploydir; 
     my $tftpdir;
@@ -12,25 +18,20 @@ sub createtftp()
     my $deployx86;
     my $deployx8664;
 
-    $kadeployuser=libkadeploy2::conflib::get_conf("deploy_user");
-    $kadeploydir=libkadeploy2::conflib::get_conf("kadeploy2_directory");
-    $tftpdir=libkadeploy2::conflib::get_conf("tftp_repository");
-    $pxedir=$tftpdir."/".libkadeploy2::conflib::get_conf("pxe_rep");
-    $tftpbootdir=$tftpdir."/".libkadeploy2::conflib::get_conf("tftp_relative_path");
+    $kadeployuser=$conf->get("deploy_user");
+    $kadeploydir=$conf->get("kadeploy2_directory");
+    $tftpdir=$conf->get("tftp_repository");
+    $pxedir=$tftpdir."/".$conf->get("pxe_rep");
+    $tftpbootdir=$tftpdir."/".$conf->get("tftp_relative_path");
     $pxelinux="$kadeploydir/lib/pxelinux/pxelinux.0";
     $memdisk="$kadeploydir/lib/pxelinux/memdisk";
     $deployx86="$kadeploydir/lib/deployment_kernel/x86/";
     $deployx8664="$kadeploydir/lib/deployment_kernel/x86_64/";
 
 
-    system("mkdir -p $tftpdir");
-    system("mkdir -p $pxedir");
-    system("mkdir -p $tftpbootdir");
+    mkdir("$tftpdir",0755);
+    mkdir("$pxedir",0755);
 
-    system("cp $pxelinux $tftpdir");
-    system("cp $memdisk  $tftpbootdir");
-    system("cp $deployx86/*   $tftpbootdir");
-    system("cp $deployx8664/* $tftpbootdir");
     system("chown -R $kadeployuser $tftpdir");
     print("Done !!!!\n");
 }
