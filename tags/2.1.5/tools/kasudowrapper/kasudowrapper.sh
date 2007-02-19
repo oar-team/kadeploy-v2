67f047e556649e1a159bbb6756a7f6cc84ebc322
leduc <leduc@ea33747e-1306-0410-90f0-ed196d91190f>
2007-02-19 16:29:47 +0000

    kasudowrapper.sh modified to launch kadeploy in a screen when not launched from a tty (kick fix to rsh bug).

--- tags/2.1.5/tools/kasudowrapper/kasudowrapper.sh
+++ tags/2.1.5/tools/kasudowrapper/kasudowrapper.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-DEPLOYDIR=/opt/kadeploy/
+DEPLOYDIR=/usr/local/kadeploy/
 DEPLOYUSER=deploy
 PERL5LIBDEPLOY=$DEPLOYDIR/share/perl/5.8
 
 export PERL5LIB=${PERL5LIBDEPLOY}/:$PERL5LIB
 export DEPLOYDIR
 
+append=""
 
-if [ -x $DEPLOYDIR/bin/`basename $0` ] ; then exec sudo -u $DEPLOYUSER $DEPLOYDIR/bin/`basename $0` "$@" ; $OK=1 ;  fi
+if [ $(basename $0) = "kadeploy" ]
+	then if $(tty -s)
+		then
+			append=${append}
+		else
+			append="screen -D -m "
+			echo -e "Kadeploy not launched from a tty\nDetached in a screen\n "
+	fi
+fi
+
+if [ -x $DEPLOYDIR/bin/`basename $0` ] ; then exec ${append} sudo -u $DEPLOYUSER $DEPLOYDIR/bin/`basename $0` "$@" ; $OK=1 ;  fi
 if [ -x $DEPLOYDIR/sbin/`basename $0` ] ; then exec sudo -u $DEPLOYUSER $DEPLOYDIR/sbin/`basename $0` "$@" ; $OK=1 ;  fi
 if [ ! $OK ] ; then echo "kasudowrapper.sh badly configured, use (prefix)/sbin/kasetup -exportenv" ; fi
 
