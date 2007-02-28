8cd709957f3b2f47e20e595856019a65fa04282f
leduc <leduc@ea33747e-1306-0410-90f0-ed196d91190f>
2007-02-28 11:02:59 +0000

    TODO: add those 2 patches on db_install

--- share/mysql/patch-kadeploy-2.1.1.sql
+++ share/mysql/patch-kadeploy-2.1.1.sql
@@ -3,7 +3,6 @@
 #
 #Environment linked to a user
 #
-CONNECT SUBSTmydeploydbSUBST;
 alter table `environment` add user varchar(255) default 'nobody';
 update environment set user='deploy' where name='grid5000';
 update environment set user='deploy' where name='debian4all';
