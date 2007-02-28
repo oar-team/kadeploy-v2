cade3747a81372c1caeeb43036b3d346d607090a
plemoine <plemoine@ea33747e-1306-0410-90f0-ed196d91190f>
2007-02-28 17:05:51 +0000

    Add nex script to create tar.gz installtion file

---
+++ create_kadeploy-2.1.5-archive.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ROOT=" AUTHORS COPYING ChangeLog FAQ INSTALL Makefile  NEWS README "
+FILES="cmd/ tools/kasudowrapper/ tools/libboot/ tools/boot tools/cookbook/install_scripts/ share/mysql/ tools/cookbook/install_scripts/ tools/cookbook/uninstall_scripts/ tools/cookbook/install_scripts/ man/src/ "
+
+TOOLS="tools/kasudowrapper/kasudowrapper.sh tools/libboot"
+
+MAN="man/src"
+
+tar czf kadeploy-2.1.5.tgz $ROOT $FILES $TOOLS --exclude=*.svn*
