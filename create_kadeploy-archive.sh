ce2b75fed903a37233f10d1b40ed5b08329d39ff
leduc <leduc@ea33747e-1306-0410-90f0-ed196d91190f>
2007-06-17 20:44:23 +0000

    txt documentation added

--- create_kadeploy-archive.sh
+++ create_kadeploy-archive.sh
@@ -3,7 +3,7 @@
 # to retrieve a clean source tree (no .svn ...):
 # svn export svn://scm.gforge.inria.fr/svn/kadeploy/<branch>
 
-ROOT=" AUTHORS COPYING changelog.txt INSTALL Makefile  NEWS README "
+ROOT=" AUTHORS COPYING changelog.txt INSTALL Makefile README "
 FILES="cmd/ tools/kasudowrapper/ tools/libboot/ tools/boot tools/cookbook/install_scripts/ share/mysql/ tools/cookbook/install_scripts/ tools/cookbook/uninstall_scripts/ tools/cookbook/install_scripts/ tools/cookbook/conf/"
 
 TOOLS="tools/kasudowrapper/kasudowrapper.sh tools/libboot"
@@ -16,6 +16,7 @@ make -C man/src/
 make -C Documentation/texi/
 # Copy files included on top of the archive
 cp Documentation/texi/changelog.txt changelog.txt
+cp Documentation/texi/INSTALL INSTALL
 #Setting execution rights to installation scripts
 chmod 755 tools/cookbook/install_scripts/*
 #Create archive
