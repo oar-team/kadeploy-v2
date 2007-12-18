443cc3abea57371b74fe210e8d95b4c1b2324e0d
dexheime <dexheime@ea33747e-1306-0410-90f0-ed196d91190f>
2007-12-18 17:09:25 +0000

    - Add a cache management for environment files (kernel, ramdisk).

---
+++ cmd/libkadeploy2/cache.pm
@@ -0,0 +1,183 @@
+package libkadeploy2::cache;
+
+# use strict;
+# use warnings;
+
+use libkadeploy2::debug;
+
+## ================
+## Global variables
+## ================
+## Tftp chroot
+my $_tftpdirectory  = "/var/lib/tftpboot/boot";
+## Directory path of Kadeploy environment files cache
+my $_cachedirectory = "$_tftpdirectory/cache";
+## days count after which files are removed from cache
+my $_expirydelay    = 30;
+## Array of files in cache
+my @_filesincache   = ();
+
+## =========
+## Functions
+## =========
+## Returns the directory path used for cached files
+sub get_cache_directory()
+{
+    return $_cachedirectory;      
+}
+
+## Returns cache directory path relative to n subdirectories from tftpdirectory
+sub get_cache_directory_tftprelative($)
+{
+    my $nsubdirs = shift;
+    
+    my $relativepath = $_cachedirectory;
+    $relativepath =~ s:^$_tftpdirectory/::;
+    
+    for (my $a = 1; $a <= $nsubdirs; $a++)
+    {
+	$relativepath = "../" . $relativepath;
+    }
+    return $relativepath;
+}
+
+## Test if cache directory exists 
+## and create it if needed
+sub test_cache()
+{
+    if (! -d $_cachedirectory) 
+    { 
+	my $success =  mkdir $_cachedirectory, 0755;
+        if ($success) { return 1; }
+        else 
+        {
+	    libkadeploy2::debug::debugl(1, "$0: mkdir failed on $_cachedirectory\n");
+	    return 0;
+	}
+    }
+    elsif ( ! -r $_cachedirectory || ! -w $_cachedirectory || ! -x $_cachedirectory ) 
+    {
+	@failed = grep !(chmod 0755, $_), $_cachedirectory;
+	if ( @failed )
+	{
+	    libkadeploy2::debug::debugl(1, "$0 : chmod failed on $_cachedirectory\n");
+	    exit 0;
+	}
+	else { return 1; }
+    }
+    else { return 1; }
+}
+
+## Read cache directory and returns files list
+sub read_files_in_cache()
+{
+    # print "cache::read_files_in_cache() \n";
+    
+    if ( ! opendir CACHEHANDLE, $_cachedirectory )
+    {
+	libkadeploy2::debug::debugl(1, "$0 : opendir failed on $_cachedirectory");
+	exit 0;
+    }
+    
+    @_filesincache = grep !/^\.\.?$/, readdir CACHEHANDLE;
+    closedir CACHEHANDLE;
+    return 1;
+}
+
+## Search a file in cache
+sub already_in_cache($)
+{
+    if ( ! @_filesincache ) { return 0; }
+    
+    my $searchedfile = shift;
+    # print "cache::already_in_cache : _filesincache = @_filesincache\n";
+    
+    if ( grep /$searchedfile/, "@_filesincache" ) { return 1; }
+    else { return 0; }
+}
+
+## Put files in cache if they're not already in
+sub put_in_cache_from_archive($$$)
+{
+    my $ref_files   = shift;
+    my @files = @{$ref_files};
+    my $archive = shift;
+    my $strip   = shift;
+    my $cachemodified = 0;
+
+    ## Test if cache directory is available
+    if ( ! test_cache() ) { return 0; }
+
+    if ( ! @_filesincache ) { read_files_in_cache(); }
+    
+    ## Clean cache from oldest files
+    libkadeploy2::cache::clean_cache();
+    
+    ## Securely add eventually new files
+    foreach my $archivefile ( @files )
+    {
+	my $cachefile = $archivefile;
+	# Strip leading directories from filename
+	$cachefile =~ s/.*\/([^\/]*)$/$1/;
+	if ( ! already_in_cache($cachefile) )
+	{
+	    # print "cache::put_in_cache_from_archive :  adding " . $archivefile . "\n";
+	    my $islink = 1;
+	    while ($islink) 
+	    {
+		libkadeploy2::debug::system_wrapper("tar -C $_cachedirectory --strip $strip -xzf $archive $archivefile");
+		$file = readlink $_cachedirectory."/".$archivefile;
+		if ( ! $file ) { $islink = 0; }
+	    }
+	    $cachemodified = 1;
+	}
+    }
+    
+    ## If cache modified, reload it
+    if ( $cachemodified ) 
+    { 
+	print "cache::put_in_cache_from_archive : reload cache\n";
+	read_files_in_cache();
+	$cachemodified = 0;
+    }
+    
+    return 1;
+}
+
+## Remove oldest files from cache
+sub clean_cache()
+{
+    my $cachemodified = 0;
+    
+    foreach my $currentfile ( @_filesincache )
+    {
+	my $f = $_cachedirectory . "/" . $currentfile;
+	my $ageoffile = ( -C $f );
+	# print "cache::clean_cache() : currentfile = " . $currentfile . " -C = " . $ageoffile . "\n";	
+	if ( $ageoffile > $_expirydelay )
+	{
+	    # print "cache::clean_cache() : delete " . $currentfile . "\n";
+	    libkadeploy2::debug::system_wrapper("rm -f $_cachedirectory/$currentfile");
+	    $cachemodified = 1;
+	}
+    }
+    
+    if ( $cachemodified ) 
+    { 
+	read_files_in_cache();
+	$cachemodified = 0;
+    }
+    
+    return 1;
+}
+
+## Remove all files from cache
+sub purge_cache()
+{
+    libkadeploy2::debug::system_wrapper("rm -rf $_cachedirectory/*");
+    read_files_in_cache();
+
+    return 1;
+}
+
+1;
