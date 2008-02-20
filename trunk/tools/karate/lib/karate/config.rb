8ed516a7496c85567d7220049ac3b256f439ba9f
ejeanvoi <ejeanvoi@ea33747e-1306-0410-90f0-ed196d91190f>
2008-02-20 10:31:53 +0000

    Added support for testing -fkr option of Kadeploy in the Karate test suite.

--- trunk/tools/karate/lib/karate/config.rb
+++ trunk/tools/karate/lib/karate/config.rb
@@ -11,12 +11,12 @@ MAX_CONCURRENT_DEPLOY = 8
 module Karate
   # Store global configuration used by all the classes of the module
   class GlobalConfig
-    attr_accessor :environment, :device, :partition, :ssh_hostname, :debug, :nb_tests, :started_date, :output_dir, :yaml
+    attr_accessor :environment, :device, :partition, :ssh_hostname, :debug, :nb_tests, :started_date, :output_dir, :yaml, :fast_kernel_reboot
     
     def initialize
       @environment = @device = @partition = @ssh_hostname = nil
       @output_dir = "."
-      @debug = false
+      @debug = @fast_kernel_reboot = false
       @nb_tests = 1
       @started_date = DateTime::now.strftime("%Y%m%d%H%M%S")
       @yaml = false
