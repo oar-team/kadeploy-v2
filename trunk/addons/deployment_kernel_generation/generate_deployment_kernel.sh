574cae18041506eecf0385e11df662040ee7aba6
dexheime <dexheime@ea33747e-1306-0410-90f0-ed196d91190f>
2008-02-25 15:21:52 +0000

    - bugfixes - management of kernel config file provided by user

--- trunk/addons/deployment_kernel_generation/generate_deployment_kernel.sh
+++ trunk/addons/deployment_kernel_generation/generate_deployment_kernel.sh
@@ -36,13 +36,9 @@ CURRENT_DIR=$( pwd )
 TODAY=$( date +%Y%m%d-%H%M-%N )
 CURRENT_BUILTDIR="$CURRENT_DIR/built-$TODAY"
 TMP_ROOTDIR="/tmp"
-TMP_RDDIR="$TMP_ROOTDIR/__buildrd"
-TMP_INITRD="$TMP_ROOTDIR/initrd.build"
+RD_MOUNT="$TMP_ROOTDIR/__mountrd"
+RD_FILE="$TMP_ROOTDIR/initrd.build"
 TMP_KERNELDIR="$TMP_ROOTDIR/__buildkernel"
-
-NCURSES_PKG='libncurses5-dev'
-CMD_SEARCH_PKG='apt-cache search'
-CMD_INSTALL_PKG='apt-get install'
 
 Die()
 {
@@ -59,29 +55,34 @@ Warn()
   echo "(WW) $1 !"
 }
 
+Banner()
+{
+  echo -e "\n\n\t=== $1 ===\n"
+}
+
 CreateInitrd()
 {
   local initrd_size=""
 
-  echo -e "\t--- Deployment ramdisk creation ---"
+  Banner "Deployment ramdisk creation"
   echo -en "- Enter INITRD size (in Ko / default = 40000 Ko) : "
   read initrdsize
   if [ -z "$initrdsize" ]; then
     initrdsize=${INITRD_SIZE}
   fi
   
-  if [ ! -d "$TMP_RDDIR" ]; then
-    mkdir "$TMP_RDDIR" || Die "Failed to create $TMP_RDDIR"
+  if [ ! -d "$RD_MOUNT" ]; then
+    mkdir "$RD_MOUNT" || Die "Failed to create $RD_MOUNT"
   fi
 
   Info "Creating empty ramdisk file"
-  dd if=/dev/zero of=${TMP_INITRD} bs=1024 count=${initrdsize} 2>&1 >/dev/null || Die "Failed to create empty ramdisk FS"
-  mkfs.ext2 -F -q ${TMP_INITRD} 2>&1 >/dev/null || Die "Failed to format $TMP_INITRD"
-  tune2fs -c 0 ${TMP_INITRD} 2>&1 >/dev/null
-  mount -o loop -t ext2 ${TMP_INITRD} ${TMP_RDDIR} || Die "Unable to mount empty ramdisk loopback file"
+  dd if=/dev/zero of=${RD_FILE} bs=1024 count=${initrdsize} 2>&1 >/dev/null || Die "Failed to create empty ramdisk FS"
+  mkfs.ext2 -F -q ${RD_FILE} 2>&1 >/dev/null || Die "Failed to format $RD_FILE"
+  tune2fs -c 0 ${RD_FILE} 2>&1 >/dev/null
+  mount -o loop -t ext2 ${RD_FILE} ${RD_MOUNT} || Die "Unable to mount empty ramdisk loopback file"
 
   Info "Decompressing ramdisk base filesystem"
-  tar -xvjf ${CURRENT_DIR}/${ROOT_FS} -C "$TMP_RDDIR/" 2>&1 >/dev/null || Die "Unable to untar ramdisk base FS"
+  tar -xvjf ${CURRENT_DIR}/${ROOT_FS} -C "$RD_MOUNT/" 2>&1 >/dev/null || Die "Unable to untar ramdisk base FS"
 }
 
 GrabKernelArchive()
@@ -90,13 +91,13 @@ GrabKernelArchive()
     mkdir "$TMP_KERNELDIR" || Die "Failed to create $TMP_KERNELDIR"
   fi
   
-  echo -e "\t--- Retrieving linux kernel ---"
+  Banner "Retrieving linux kernel"
   echo -en "- Enter wanted version of kernel : "
 
   while true; do
     read KERNEL_VERSION
     if [ -f "$CURRENT_DIR/linux-$KERNEL_VERSION.tar.bz2" ]; then
-      echo "(II) linux-$KERNEL_VERSION already grabbed"
+      Info "linux-$KERNEL_VERSION already grabbed"
       break
     else
       wget --verbose --progress=bar "${KERNEL_2_6_ARCHIVE_URL}/linux-${KERNEL_VERSION}.tar.bz2"
@@ -104,87 +105,95 @@ GrabKernelArchive()
         break
       else
         echo 
-        echo "(WW) Previous entry is not a valid version !"
+        Warn "Previous entry is not a valid version !"
         echo -en "- Please enter a valid kernel version number : "
       fi
     fi
   done
   
   Info "Decompressing kernel archive"
-  ( bzip2 -cd $CURRENT_DIR/linux-${KERNEL_VERSION}.tar.bz2|tar -C $TMP_KERNELDIR -xvf - 2>&1 >/dev/null ) || Die "Failed to decompress kernel archive"
+  ( bzip2 -cd $CURRENT_DIR/linux-${KERNEL_VERSION}.tar.bz2|tar -C $TMP_KERNELDIR -xvf - 2>&1 >/dev/null ) || ( CleanOut && Die "Failed to decompress kernel archive" )
 
-}
-
-CheckNcurses()
-{
-  local installed = $( "${CMD_SEARCH_PKG}" "${NCURSES_PKG}" )
-  ( [ -z "$installed" ] && "${CMD_INSTALL_PKG}" "${NCURSES_PKG}" ) || Die "Failed to install ${NCURSES_PKG}"
 }
 
 BuildKernel()
 {  
-  echo -e "\t--- Kernel build ---"
+  Banner "Kernel build"
 
-  echo -en "- Enter pathname to a kernel config file : "
+  echo -en "- Enter pathname of a kernel config file or [Return] otherwise (use defaults) : "
   read configfile
-  if [ -z "$configfile" ]; then
-    CheckNcurses
-    echo "(II) Trying to launch kernel configuration utility..."
-    ( cd $TMP_KERNELDIR/linux-${KERNEL_VERSION} && make menuconfig ) || Die "Failed to run kernel menuconfig"
-    cp $TMP_KERNELDIR/linux-${KERNEL_VERSION}/.config ${CURRENT_DIR}/config-${KERNEL_VERSION}
-  else
-    while true; do
-      if [ ! -f "$configfile" ]; then
-        echo
-        echo "(WW) $configfile is not a valid config file."
-        echo -en "- Please enter a valid kernel config file : "
-	read $configfile
-      else
-        cp ${configfile} ${TMP_KERNELDIR}/linux-${KERNEL_VERSION}/.config
-        break
-      fi
-    done
- fi
-  
+
+  while true; do
+    if [ -z "$configfile" ]; then
+      Info "Trying to launch kernel configuration utility..."
+      ( cd ${TMP_KERNELDIR}/linux-${KERNEL_VERSION} && make menuconfig ) || ( CleanOut && Die "Failed to run kernel menuconfig" )
+      break
+    elif [ ! -f "$configfile" ]; then
+       Warn "$configfile : not a valid kernel config file"
+       echo -en "- Enter pathname of a kernel config file or [Return] otherwise (use defaults) : "
+       read configfile
+    else
+      echo -en "Do you wish to setup manually new kernel options (Y/n) ? "
+      read rep
+      case "$rep" in
+        N|n|No|no|non|Non)
+	  ( cd ${TMP_KERNELDIR}/linux-${KERNEL_VERSION} && KCONFIG_ALLCONFIG=${configfile} make allmodconfig 2>&1 >/dev/null ) || ( CleanOut && Die "Failed to run : make allmodcofig" )
+	  ;;
+	*)
+	  cp ${configfile} ${TMP_KERNELDIR}/linux-${KERNEL_VERSION}/.config
+	  ( cd ${TMP_KERNELDIR}/linux-${KERNEL_VERSION} && make oldconfig ) || ( CleanOut && Die "Failed to run kernel menuconfig" )
+	  ;;
+      esac
+      break
+    fi
+  done
+
   cd ${TMP_KERNELDIR}/linux-${KERNEL_VERSION}/
   Info "Making bzImage"
-  make -j$COMPILATION_PARALLELISM bzImage 2>&1 >/dev/null || Die "Failed to make bzImage"
+  make -j$COMPILATION_PARALLELISM bzImage 2>&1 >/dev/null || ( CleanOut && Die "Failed to make bzImage" )
   
   Info "Making modules"
-  make -j$COMPILATION_PARALLELISM modules 2>&1 >/dev/null || Die "Failed to make modules"
+  make -j$COMPILATION_PARALLELISM modules 2>&1 >/dev/null || ( CleanOut && Die "Failed to make modules" )
   
   Info "Making kernel install to ${CURRENT_BUILTDIR}"
-  INSTALL_PATH=$CURRENT_BUILTDIR make install  || Die "Failed to make kernel installation in $CURRENT_BUILTDIR"
+  INSTALL_PATH=${CURRENT_BUILTDIR} make install  || ( CleanOut && Die "Failed to make kernel installation in ${CURRENT_BUILTDIR}" )
 
-  Info "Making modules install to ${TMP_RDDIR}"
-  INSTALL_MOD_PATH=$TMP_RDDIR make modules_install || Die "Failed to make modules installation in $TMP_RDDIR"
+  Info "Making modules install to ${RD_MOUNT}"
+  INSTALL_MOD_PATH=${RD_MOUNT} make modules_install || ( CleanOut && Die "Failed to make modules installation in ${RD_MOUNT}" )
 
   cp ${CURRENT_BUILTDIR}/vmlinuz-${KERNEL_VERSION} ${CURRENT_BUILTDIR}/${OUTPUT_KERNEL}-${KERNEL_VERSION}
   rdev ${CURRENT_BUILTDIR}/${OUTPUT_KERNEL}-${KERNEL_VERSION} ${INITRD_ROOTDEVICE}
-  cd "${TMP_RDDIR}/lib/modules" && depmod -b ${TMP_RDDIR} ${KERNEL_VERSION}
+  cd "${RD_MOUNT}/lib/modules" && depmod -b ${RD_MOUNT} ${KERNEL_VERSION}
   
+  Info "Deployment kernel built : ${CURRENT_BUILTDIR}/${OUTPUT_KERNEL}-${KERNEL_VERSION}"
+  Info "The kernel config file used for building : ${CURRENT_BUILTDIR}/config-${KERNEL_VERSION}"
 }
 
 BuildInitrd()
 {
-  cd ${CURRENT_DIR} && umount ${TMP_RDDIR}
-  cat ${TMP_INITRD} | gzip -9 -c > "${CURRENT_BUILTDIR}/${OUTPUT_INITRD}-${KERNEL_VERSION}"
+  cd ${CURRENT_DIR} && umount ${RD_MOUNT}
+  cat ${RD_FILE} | gzip -9 -c > "${CURRENT_BUILTDIR}/${OUTPUT_INITRD}-${KERNEL_VERSION}"
+  
+  Info "Ramdisk built : ${CURRENT_BUILTDIR}/${OUTPUT_INITRD}-${KERNEL_VERSION}"
 }
 
 
 CleanOut()
 {
-  echo -e "\t--- Cleaning out temporary files ---"
+  Banner "Cleaning out temporary files"
 
-  local mounted=$( mount|grep $TMP_RDDIR )
+  local mounted=$( mount|grep $RD_MOUNT )
   if [ -n "$mounted" ]; then
-    ( cd ${CURRENT_DIR} && umount ${TMP_RDDIR} ) || Warn "Failed to unmount ${TMP_RDDIR}"
+    ( cd ${CURRENT_DIR} && umount ${RD_MOUNT} ) || Warn "Failed to unmount ${RD_MOUNT}"
   fi
-  rm -rf $TMP_KERNELDIR || Warn "Failed to remove kernel build directory $TMP_KERNELDIR"
-  rm -rf $TMP_RDDIR || Warn "Failed to remove initrd build directory $TMP_RDDIR"
+  rm -rf ${TMP_KERNELDIR} || Warn "Failed to remove kernel build directory ${TMP_KERNELDIR}"
+  rm -rf ${RD_MOUNT} || Warn "Failed to remove initrd build directory ${RD_MOUNT}"
+  rm -f ${RD_FILE} || Warn "Failed to remove initrd loopback file ${RD_FILE}"
   # rm -f "${CURRENT_BUILTDIR}/vmlinuz*" 2>&1 >/dev/null || Warn "Failed to rm ${CURRENT_BUILTDIR}/vmlinuz* files" 
   # rm -f "${CURRENT_BUILTDIR}/System.map*" 2>&1 >/dev/null || Warn "Failed to rm ${CURRENT_BUILTDIR}/System.map* files"
   
+  return 0
+
 }
 
 Exit_handler()
