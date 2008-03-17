620a9ea26cfc405f11de2f1a0df7ea817fe60576
dexheime <dexheime@ea33747e-1306-0410-90f0-ed196d91190f>
2008-03-17 16:45:02 +0000

    - Support for booting multiboot environment (Xen Linux) (port of a patch from Aurelien C. for Kadeploy 2.1.6) - Module for grouping pathnames operations (pathlib)

--- trunk/test/test_generate_nogrub_files.pl
+++ trunk/test/test_generate_nogrub_files.pl
@@ -20,18 +20,25 @@ print "kernel = " . $kernel . "\n";
 $kernel =~ s/.*\/([^\/]*)$/$1/;
 print "kernel = " . $kernel . "\n";
 
-my $img_archive = "/grid5000/images/sid32bits-ejeanvoine8.tgz";
-my $img_kernel = "/boot/vmlinuz-2.6.21.3";
-my $img_initrd = "/boot/initrd.img-2.6.21.3";
-my $dest_dir = "/var/lib/tftpboot/AABBCC01";
+my $img_archive = "/home/nancy/bdexheimer/images/etch-x64-base-xen-1.0.tgz";
+my $img_kernel = "mboot.c32";
+my $img_initrd = "/boot/xen-3.0.3-1-amd64.gz dom0_mem=262144 --- /boot/vmlinuz-2.6.18-6-xen-amd64 --- /boot/initrd.img-2.6.18-6-xen-amd64";
+my $dest_dir = "/var/lib/tftpboot/boot/AABBCC01";
 my $firstipx = "AABBCC01";
 my $firstnode = 1;
-libkadeploy2::bootlib::generate_nogrub_files($img_archive, $img_kernel, $img_initrd, $dest_dir, $firstipx, $firstnode);
+my $envid = 123;
 
-my $dest_dir = "/var/lib/tftpboot/AABBCC02";
-my $firstnode = 0;
-libkadeploy2::bootlib::generate_nogrub_files($img_archive, $img_kernel, $img_initrd, $dest_dir, $firstipx, $firstnode);
+libkadeploy2::bootlib::generate_nogrub_files($img_archive, $img_kernel, $img_initrd, $dest_dir, $firstipx, $firstnode, $envid);
 
-my $dest_dir = "/var/lib/tftpboot/AABBCC03";
-my $firstnode = 0;
-libkadeploy2::bootlib::generate_nogrub_files($img_archive, $img_kernel, $img_initrd, $dest_dir, $firstipx, $firstnode);
+my $f = "xen kernel initrd";
+my @tf = qw/ xen kernel initrd /;
+my $f2 = shift(@tf);
+print "f = ".$f." tf = ".@tf." f2 = ".$f2."\n";
+
+# my $dest_dir = "/var/lib/tftpboot/AABBCC02";
+# my $firstnode = 0;
+# libkadeploy2::bootlib::generate_nogrub_files($img_archive, $img_kernel, $img_initrd, $dest_dir, $firstipx, $firstnode);
+
+# my $dest_dir = "/var/lib/tftpboot/AABBCC03";
+# my $firstnode = 0;
+# libkadeploy2::bootlib::generate_nogrub_files($img_archive, $img_kernel, $img_initrd, $dest_dir, $firstipx, $firstnode);
