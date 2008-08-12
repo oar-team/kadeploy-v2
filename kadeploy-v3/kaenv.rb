0b1e2fda24ebbeb7e0ee449c490532c70eb1c51f
ejeanvoi <ejeanvoi@ea33747e-1306-0410-90f0-ed196d91190f>
2008-08-12 13:12:11 +0000

    Completed the implementation of Kaenv.

--- kadeploy-v3/kaenv.rb
+++ kadeploy-v3/kaenv.rb
@@ -1,8 +1,88 @@
 #!/usr/bin/ruby -w
 require 'lib/config'
 require 'lib/db'
+require 'lib/environment'
 
 
+def list_environments(config, db)
+  env = EnvironmentManagement::Environment.new
+  if (config.exec_specific.show_all_version == false) then
+    query = "SELECT * FROM environment WHERE user=\"#{config.exec_specific.user}\" \
+                                       AND version=(SELECT MAX(version) FROM environment WHERE user=\"#{config.exec_specific.user}\")"
+  else
+    query = "SELECT * FROM environment WHERE user=\"#{config.exec_specific.user}\"
+                                       ORDER BY version"
+  end
+  res = db.run_query(query)
+  env.short_view_header
+  res.each_hash { |row|
+    env.load_from_hash(row)
+    env.short_view
+  }
+end
+
+def add_environment(config, db)
+  env = EnvironmentManagement::Environment.new
+  env.load_from_file(config.exec_specific.file)
+  query = "INSERT INTO environment (name, \
+                                    version, \
+                                    description, \
+                                    author, \
+                                    tarball_file, \
+                                    tarball_md5, \
+                                    postinstall_file, \
+                                    postinstall_md5, \
+                                    kernel, \
+                                    kernel_params, \
+                                    initrd, \
+                                    part, \
+                                    fdisk_type, \
+                                    filesystem,user) \
+                            VALUES (\"#{env.name}\", \
+                                    \"#{env.version}\", \
+                                    \"#{env.description}\", \
+                                    \"#{env.author}\", \
+                                    \"#{env.tarball_file}\", \
+                                    \"#{env.tarball_md5}\", \
+                                    \"#{env.postinstall_file}\", \
+                                    \"#{env.postinstall_md5}\", \
+                                    \"#{env.kernel}\", \
+                                    \"#{env.kernel_params}\", \
+                                    \"#{env.initrd}\", \
+                                    \"#{env.part}\", \
+                                    \"#{env.fdisk_type}\", \
+                                    \"#{env.filesystem}\", \
+                                    \"#{env.user}\")"
+  db.run_query(query)
+end
+
+def delete_environment(config,db)
+  env = EnvironmentManagement::Environment.new
+  query = "DELETE FROM environment WHERE name=\"#{config.exec_specific.env_name}\" \
+                                     AND user=\"#{ENV['USER']}\""
+  db.run_query(query)
+end
+
+def print_environment(config, db)
+  env = EnvironmentManagement::Environment.new
+  if (config.exec_specific.show_all_version == false) then
+    query = "SELECT * FROM environment WHERE name=\"#{config.exec_specific.env_name}\" \
+                                       AND user=\"#{config.exec_specific.user}\" \
+                                       AND version=(SELECT MAX(version) FROM environment WHERE user=\"#{config.exec_specific.user}\")"
+  else
+    query = "SELECT * FROM environment WHERE name=\"#{config.exec_specific.env_name}\" \
+                                       AND user=\"#{config.exec_specific.user}\"
+                                       ORDER BY version"
+  end
+  res = db.run_query(query)
+  if res != nil then
+    res.each_hash { |row|
+      puts "###"
+      env.load_from_hash(row)
+      env.full_view
+    }
+  end
+end
 
 config = ConfigInformation::Config.new("kaenv")
 if (config.check_config("kaenv") == true)
@@ -11,7 +91,16 @@ if (config.check_config("kaenv") == true)
              config.common.deploy_db_login,
              config.common.deploy_db_passwd,
              config.common.deploy_db_name)
-  res = db.run_query("SELECT * from environment")
 
+  case config.exec_specific.operation  
+  when "list"
+    list_environments(config, db)
+  when "add"
+    add_environment(config, db)
+  when "delete"
+    delete_environment(config,db)
+  when "print"
+    print_environment(config, db)
+  end
   db.disconnect
 end
--- kadeploy-v3/lib/environment.rb
+++ kadeploy-v3/lib/environment.rb
@@ -1,16 +1,24 @@
 require 'digest/md5'
+require 'lib/db'
 
 module EnvironmentManagement
   class Environment
+    attr_reader :name
+    attr_reader :version
+    attr_reader :description
+    attr_reader :author
     attr_reader :tarball_file
     attr_reader :tarball_md5
     attr_reader :postinstall_file
     attr_reader :postinstall_md5
     attr_reader :kernel
-    attr_reader :initrd
     attr_reader :kernel_params
+    attr_reader :initrd
     attr_reader :part
-   
+    attr_reader :fdisk_type
+    attr_reader :filesystem
+    attr_reader :user
+
     def load_from_file(file)
       if not File.exist?(file)
         raise "#{file} does not exist"
@@ -21,6 +29,14 @@ module EnvironmentManagement
             attr = content[1]
             val = content[2]
             case attr
+            when "name"
+              @name = val
+            when "version"
+              @version = val
+            when "description"
+              @description = val
+            when "author"
+              @author = val
             when "tarball_file"
               @tarball_file = val
             when "tarball_md5"
@@ -31,12 +47,18 @@ module EnvironmentManagement
               @postinstall_md5 = val
             when "kernel"
               @kernel = val
-            when "initrd"
-              @initrd = val
             when "kernel_params"
               @kernel_params = val
+            when "initrd"
+              @initrd = val
             when "part"
               @part = val
+            when "fdisk_type"
+              @fdisk_type = val
+            when "filesystem"
+              @filesystem = val
+            when "user"
+              @user = val
             else
               puts "#{attr} is an invalid attribute"
             end
@@ -46,22 +68,59 @@ module EnvironmentManagement
       check_md5_digest
     end
 
+    def load_from_database(name, version, user)
+      
+    end
+
+    def load_from_hash(hash)
+      @name = hash["name"]
+      @version = hash["version"]
+      @description = hash["description"]
+      @author = hash["author"]
+      @tarball_file = hash["tarball_file"]
+      @tarball_md5 = hash["tarball_md5"]
+      @postinstall_file = hash["postinstall_file"]
+      @postinstall_md5 = hash["postinstall_md5"]
+      @kernel = hash["kernel"]
+      @kernel_params = hash["kernel_params"]
+      @initrd = hash["initrd"]
+      @part = hash["part"]
+      @fdisk_type = hash["fdisk_type"]
+      @filesystem = hash["filesystem"]
+      @user = hash["user"]
+    end
+
     def check_md5_digest
       if not ((Digest::MD5.hexdigest(File.read(@tarball_file)) == @tarball_md5) && (Digest::MD5.hexdigest(File.read(@postinstall_file)) == @postinstall_md5))
         raise "Invalid md5 checksum"
       end
     end
 
-    def load_from_database
-
+    def short_view_header
+      puts "Name         Version     User            Description"
+      puts "####         #######     ####            ###########"
     end
 
-    def to_s
-      puts "Tarball: #{@tarball_file}, md5: #{@tarball_md5}"
-      puts "Postinstall: #{@postinstall_file}, md5: #{@postinstall_md5}"
-      puts "Kernel: #{@kernel}"
-      puts "Initrd: #{@initrd}"
-      puts "Partition: #{@part}"
+    def short_view
+      printf("%-15s %-7s %-10s %-40s\n", @name, @version, @user, @description)
+    end
+
+    def full_view
+      puts "name : #{@name}"
+      puts "version : #{@version}"
+      puts "description : #{@description}"
+      puts "author : #{@author}"
+      puts "tarball_file : #{@tarball_file}"
+      puts "tarball_md5 : #{@tarball_md5}"
+      puts "postinstall_file : #{@postinstall_file}"
+      puts "postinstall_md5 : #{@postinstall_md5}"
+      puts "kernel : #{@kernel}"
+      puts "kernel_params : #{@kernel_params}"
+      puts "initrd : #{@initrd}"
+      puts "part : #{@part}"
+      puts "fdisk_type : #{@fdisk_type}"
+      puts "filesystem : #{@filesystem}"
+      puts "user : #{@user}"
     end
   end
 end
