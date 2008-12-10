e0cd31bb7b7e2275cf1da0f2d58a12d46ed59e77
ejeanvoi <ejeanvoi@ea33747e-1306-0410-90f0-ed196d91190f>
2008-12-10 14:56:07 +0000

    Added and modified some information for the debootstrap configuration.

--- branches/ruby/kadeploy-v3/addons/deploy_env_generation/debootstrap/scripts/launch_transfert.sh
+++ branches/ruby/kadeploy-v3/addons/deploy_env_generation/debootstrap/scripts/launch_transfert.sh
@@ -3,4 +3,4 @@
 # build chain as root using a detached program
 # can be run as many times as there are transfers because the chain can be rebuilt
 
-start-stop-daemon -S -x /usr/local/bin/build_chain.sh -b -- $1
+/sbin/start-stop-daemon -S -x /usr/local/bin/build_chain.sh -b -- $1
