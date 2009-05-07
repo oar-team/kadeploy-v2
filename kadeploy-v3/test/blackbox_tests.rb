bdb3e38ce9fb9cbbfaa0cb08b9ef79fa348d9d63
ejeanvoi <ejeanvoi@ea33747e-1306-0410-90f0-ed196d91190f>
2009-05-07 14:38:23 +0000

    Modified the blackbox test suite.

--- kadeploy-v3/test/blackbox_tests.rb
+++ kadeploy-v3/test/blackbox_tests.rb
@@ -1,12 +1,9 @@
 #!/usr/bin/ruby -w
-require 'lib/cmdctrl_wrapper'
-require 'lib/nodes'
 require 'optparse'
 
-KADEPLOY="/home/ejeanvoi/work/kadeploy-svn/branches/ruby/kadeploy-v3/kadeploy_client.rb"
-ENVIRONMENT="etch test"
-TEST_CMD="/test_env.sh"
-EXPECTED_VALUE="Cool!"
+KADEPLOY="kadeploy3"
+ENV_LIST="sid-x64-base-1.1,freebsd7"
+MAX_SIMULTANEOUS_DEPLOY=8
 
 def load_cmdline_options
   nodes = Array.new
@@ -39,76 +36,108 @@ def load_cmdline_options
   opts.parse!(ARGV)
   return nodes, key
 end
+  
 
-
-def test_deploy(nodes, step1, step2, step3, tag)
-  puts "### Launching #{tag} test ###"
-  File.delete("nodes_ok") if File.exist?("nodes_ok")
+def _test_deploy(nodes, step1, step2, step3, test_name, key, env, ok = "nodes_ok", ko = "nodes_ko")
+  puts "# Launching test #{test_name} with #{env} env"
+  File.delete(ok) if File.exist?(ok)
+  File.delete(ko) if File.exist?(ko)
   node_list = String.new
   nodes.each { |node|
     node_list += " -m #{node}"
   }
-  cmd = "#{KADEPLOY} #{node_list} -e \"#{ENVIRONMENT}\" -d 0 -z \"SetDeploymentEnv|#{step1}&BroadcastEnv|#{step2}&BootNewEnv|#{step3}\""
+  cmd = "#{KADEPLOY} #{node_list} -e \"#{env}\" -d 0 -k #{key} --force-steps \"SetDeploymentEnv|#{step1}&BroadcastEnv|#{step2}&BootNewEnv|#{step3}\" -o #{ok} -n #{ko}"
   system(cmd)
-  if File.exist?("nodes_ko") then
-    IO.readlines("nodes_ko").each { |node|
-      puts "The node #{node} has not been correctly deployed"
-    }
-  else
-    if File.exist?("nodes_ok") then
-      puts "[ PASSED ]"
-    else
-      puts "[ ERROR ]"
-    end
-  end
-end    
-
-def test_connection(nodes, step1, step2, step3, key)
-  puts "### Launching Connection test ###"
-  File.delete("nodes_ok") if File.exist?("nodes_ok")
-  node_list = String.new
-  nodes.each { |node|
-    node_list += " -m #{node}"
-  }
-  cmd = "#{KADEPLOY} #{node_list} -e \"#{ENVIRONMENT}\" -d 0 -k #{key} -z \"SetDeploymentEnv|#{step1}&BroadcastEnv|#{step2}&BootNewEnv|#{step3}\""
-  system(cmd)
-  if File.exist?("nodes_ko") then
-    IO.readlines("nodes_ko").each { |node|
+  if File.exist?(ko) then
+    IO.readlines(ko).each { |node|
       puts "The node #{node} has not been correctly deployed"
     }
   end
-  if File.exist?("nodes_ok") then
+  if File.exist?(ok) then
     deployed_nodes = Array.new
-    IO.readlines("nodes_ok").each { |node|
+    IO.readlines(ok).each { |node|
       deployed_nodes.push(node.chomp)
     }
-    pr = CmdCtrlWrapper::init
+    results = Hash.new
     deployed_nodes.each { |node|
-      cmd = "ssh -q -o BatchMode=yes -o StrictHostKeyChecking=no -o PreferredAuthentications=publickey -o ConnectTimeout=2 root@#{node} \"#{TEST_CMD}\""
-      node_instance = Nodes::Node.new(node,"","","")
-      CmdCtrlWrapper::add_cmd(pr, cmd, node_instance)
+      cmd = "ssh -q -o BatchMode=yes -o StrictHostKeyChecking=no -o PreferredAuthentications=publickey -o ConnectTimeout=2 root@#{node} \"true\""
+      res = system(cmd)
+      results[node] = res
     }
-    CmdCtrlWrapper::run(pr)
-    res = CmdCtrlWrapper::get_results_expecting_output(pr, EXPECTED_VALUE)
-    if (res[1].length > 0) then
-      res[1].each { |node_instance|
-        puts "Error on the node #{node_instance.hostname}"
-        puts "[ ERROR ]"
-      }
-    else
-      puts "[ PASSED ]"
-    end
+    no_errors = true
+    results.each_pair { |node,res|
+      if not res then
+        puts "Connection error on the node #{node}"
+        no_errors = false
+      end
+    }
+    return no_errors
   end
 end
 
+def test_deploy(nodes, step1, step2, step3, test_name, key)
+  ENV_LIST.split(",").each { |env|
+    start = Time.now.to_i
+    if _test_deploy(nodes, step1, step2, step3, test_name, key, env) then
+      puts "[ PASSED ] (#{Time.now.to_i - start}s)"
+    else
+      puts "[ ERROR ] (#{Time.now.to_i - start}s)"
+    end
+  }
+end
+
+def test_simultaneous_deployments(nodes, step1, step2, step3, test_name, key)
+  simult = 2
+  while ((simult <= nodes.length()) && (simult <= MAX_SIMULTANEOUS_DEPLOY)) do
+    start = Time.now.to_i
+    puts "*** Performing #{simult} simultaneous deployments"
+    nodes_hash = Hash.new
+    (0...simult).to_a.each { |n|
+      nodes_hash[n] = Array.new
+    }
+    nodes.each_index { |i|
+      nodes_hash[i.modulo(simult)].push(nodes[i])
+    }
+    tid_array = Array.new
+    (0...simult).to_a.each { |n|
+      tid_array << Thread.new {
+        _test_deploy(nodes_hash[n], step1, step2, step3, test_name, key, ENV_LIST.split(",")[0], "nodes_ok_#{n}", "nodes_ko_#{n}")
+      }
+    }
+    result = true
+    tid_array.each { |tid|
+      if not tid.value then
+        result = false
+      end
+    }
+    if result then
+      puts "[ PASSED ] (#{Time.now.to_i - start}s)"
+    else
+      puts "[ ERROR ] (#{Time.now.to_i - start}s)"
+    end
+    simult += 2
+  end
+end
 
 nodes, key = load_cmdline_options
 if nodes.empty? then
   puts "You must specify at least on node, use --help option for correct use"
-else
-  test_deploy(nodes, "SetDeploymentEnvDummy:1:10", "BroadcastEnvDummy:1:10", "BootNewEnvDummy:1:10", "Dummy")
-  test_deploy(nodes, "SetDeploymentEnvProd:2:100", "BroadcastEnvChainWithFS:2:300", "BootNewEnvKexec:1:150", "ProdEnv - Kexec reboot")
-  test_deploy(nodes, "SetDeploymentEnvUntrusted:2:200", "BroadcastEnvChainWithFS:2:300", "BootNewEnvKexec:1:150", "UntrustedEnv - Kexec reboot")
-  test_deploy(nodes, "SetDeploymentEnvUntrusted:2:200", "BroadcastEnvChainWithFS:2:300", "BootNewEnvClassical:1:150", "UntrustedEnv - Classical reboot")
-  test_connection(nodes, "SetDeploymentEnvUntrusted:2:200", "BroadcastEnvChainWithFS:2:300", "BootNewEnvKexec:1:150", key) if (key != "")
+  exit(1)
 end
+if (key == "") || (not File.readable?(key)) then
+  puts "You must specify an SSH public key (a readable file), use --help option for correct use"
+  exit(1)
+end
+
+puts "----------- Simple deploy tests -------------"
+#  test_deploy(nodes, "SetDeploymentEnvDummy:1:10", "BroadcastEnvDummy:1:10", "BootNewEnvDummy:1:10", "Dummy", key)
+#  test_deploy(nodes, "SetDeploymentEnvProd:2:100", "BroadcastEnvChainWithFS:2:300", "BootNewEnvKexec:1:150", "ProdEnv - Kexec reboot")
+#  test_deploy(nodes, "SetDeploymentEnvUntrusted:1:500", "BroadcastEnvChain:1:400", "BootNewEnvKexec:1:400", "UntrustedEnv - Taktuk broadcast - Kexec reboot", key)
+test_deploy(nodes, "SetDeploymentEnvUntrusted:1:500", "BroadcastEnvChain:1:400", "BootNewEnvClassical:1:500", "UntrustedEnv - Taktuk broadcast - Classical reboot", key)
+#  test_deploy(nodes, "SetDeploymentEnvUntrusted:1:500", "BroadcastEnvKastafior:1:400", "BootNewEnvKexec:1:400", "UntrustedEnv - Kastafior broadcast - Kexec reboot", key)
+test_deploy(nodes, "SetDeploymentEnvUntrusted:1:500", "BroadcastEnvKastafior:1:400", "BootNewEnvClassical:1:500", "UntrustedEnv - Kastafior broadcast - Classical reboot", key)
+
+puts "-------- Simultaneous deploy tests ----------"
+test_simultaneous_deployments(nodes, "SetDeploymentEnvUntrusted:1:500", "BroadcastEnvKastafior:1:400", "BootNewEnvClassical:1:500", "UntrustedEnv - Kastafior broadcast - Classical reboot", key)
+
+exit 0
