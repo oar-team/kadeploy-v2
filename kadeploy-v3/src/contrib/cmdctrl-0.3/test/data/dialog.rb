da93ce3a63ac1c4ce9a0bccaf98c142c970952b2
ejeanvoi <ejeanvoi@ea33747e-1306-0410-90f0-ed196d91190f>
2009-06-09 08:41:49 +0000

    Removed CmdCtrl from the repository.

--- kadeploy-v3/src/contrib/cmdctrl-0.3/test/data/dialog.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-
-(1..5).each do |i|
-  puts "Question #{i} ?"
-  veux = gets
-  puts "Read: #{veux}"
-end
-
--- kadeploy-v3/src/contrib/cmdctrl-0.3/test/data/dialog_err.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/ruby
-
-(1..5).each do |i|
-  STDERR.puts "Question #{i} ?"
-  veux = gets
-  STDERR.puts "Read: #{veux}"
-end
