27d695fbd6e6a4c95f3588563e4184b7b11065bd
dexheime <dexheime@ea33747e-1306-0410-90f0-ed196d91190f>
2009-09-07 13:32:20 +0000

    - duplicated files for tag 2.1.8 : removed

--- tags/2.1.8/scripts/scripts_used_to_deploy/wait_background.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-FILE_LOCK=$1
-
-while [ -f ${FILE_LOCK} ]; do
-	sleep 1
-done
--- tags/2.1.8/tools/karate/lib/cmdctrl/cmdctrl/commands.rb
+++
@@ -1,3 +0,0 @@
-require 'cmdctrl/commands/command_bufferer'
-require 'cmdctrl/commands/command'
-require 'cmdctrl/commands/interactive_command'
